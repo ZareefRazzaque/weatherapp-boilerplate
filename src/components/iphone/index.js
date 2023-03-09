1bb8510bb34e4e95583da2da54511218e4fcfe81
Zareef Razzaque <ZareefRazzaque@outlook.com>
2023-03-09 18:47:19 +0000

    adding comments

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -19,10 +19,24 @@ export default class Iphone extends Component {
 		// button display state
 		this.setState({ display: true });
 	}
+	//function assigns variable values from the json file to ones that can be used in the application    
+	parseResponse = (parsed_json) => {
+
+		var location = parsed_json['name']; //name of the location saved here 	
+		var temp_c = parsed_json['main']['temp']; // temperature conditions are stored here
+		var conditions = parsed_json['weather']['0']['description'];//the current weather conditions are here (eg cloudy, rain , sunny etc)
+
+		// set states for fields so they could be rendered later on
+		this.setState({
+			locate: location,
+			temp: temp_c,
+			cond : conditions
+		});      
+	}
 
 	// a call to fetch weather data via wunderground
 	fetchWeatherData = () => {
-		// API URL with a structure of : ttp://api.wunderground.com/api/key/feature/q/country-code/city.json
+		// API URL with a structure of : http://api.wunderground.com/api/key/feature/q/country-code/city.json
 		var url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=bff435938a5989963d8a821ee442e57f";
 		$.ajax({
 			url: url,
@@ -46,8 +60,8 @@ export default class Iphone extends Component {
 		// display all weather data
 		return (
 			<div class={ style.container }>
-				<div class={ style.header }>
-					<div class={ style.city }>{ this.state.locate }</div>
+				<div class={ style.header }> 
+					<div class={ style.city }>{ this.state.locate }</div>			<!-- these won't show until data is fetched>
 					<div class={ style.conditions }>{ this.state.cond }</div>
 					<span class={ tempStyles }>{ this.state.temp }</span>
 				</div>
@@ -63,16 +77,5 @@ export default class Iphone extends Component {
 
 	
 
-	parseResponse = (parsed_json) => {
-		var location = parsed_json['name'];
-		var temp_c = parsed_json['main']['temp'];
-		var conditions = parsed_json['weather']['0']['description'];
 
-		// set states for fields so they could be rendered later on
-		this.setState({
-			locate: location,
-			temp: temp_c,
-			cond : conditions
-		});      
-	}
 }
