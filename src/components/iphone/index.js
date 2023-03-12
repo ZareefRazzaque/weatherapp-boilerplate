d1777bc3a0f0d79c83d2ddf6e3e7e85266580c6b
Zareef Razzaque <zareefrazzaque@outlook.com>
2023-03-12 18:29:31 +0000

    fixing some errors

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -107,7 +107,7 @@ export default class Iphone extends Component {
 
 
 					{/*custom widgets class here, they require a position   */}
-					{<Widget originalheight={200} howleft= {"220px"} input ={basicTemperature}></Widget> }
+					{<Widget originalheight={200} howleft= {"220px"} input ={basicTemperature} clickeddata = {"testing"}></Widget> }
 					{<Widget originalheight={470} howleft= {"20px"} input ={basicWind}></Widget> }
 					{<Widget originalheight={200} howleft= {"20px"} input ={basicpercipitaiton}></Widget> }
 					{<Widget originalheight={470} howleft= {"220px"} input ={basicWeather}></Widget> }
--- src/components/widget/index.js
+++ src/components/widget/index.js
@@ -4,35 +4,58 @@ import widget_style from './widget_style.less'
 export default class Widget extends Component {
 
     constructor(props){
-        super(props)
+        super(props);
 
         this.state = {
             scrolling: false,
             ychange:0,
             yinitial:0,
-            position:this.props.originalheight
+            position: this.props.originalheight,
+            selected:false,
+
         }
 
 
-        this.noticeTouch = this.noticeTouch.bind(this)
-        this.ifScrolling = this.ifScrolling.bind(this)
-        this.notTouching = this.notTouching.bind(this)
-        document.addEventListener('mousedown', this.noticeTouch)
-        document.addEventListener('mousemove', this.ifScrolling)
-        document.addEventListener('mouseup', this.notTouching)
+        this.noticeTouch = this.noticeTouch.bind(this);
+        this.ifScrolling = this.ifScrolling.bind(this);
+        this.notTouching = this.notTouching.bind(this);
+        document.addEventListener('mousedown', this.noticeTouch);
+        document.addEventListener('mousemove', this.ifScrolling);
+        document.addEventListener('mouseup', this.notTouching);
         
 
+        
+        this.startclick = this.startclick.bind(this);
+        this.checkclick = this.checkclick.bind(this);
+        addEventListener('mouseup', this.checkclick);
     }
+    startclick(event){
+        this.setState({
+            originalposition: this.state.position,
+            selected:true
+        })
 
 
+    }
+    
+    checkclick(event){
+        if (this.state.selected == true){
+            if (this.state.originalposition == this.state.position){
+                console.log("test")
+            }
+        }
+        this.setState({
+            selected:false,
+            whenclicked:true,
+        })
+    }
 
     //checks to see if the user has touched the screen 
     noticeTouch(event){
         this.setState({
             scrolling:true,
             yinitial: event.clientY //takes details of where the user touched the screen
-
-        })
+        });
 
     }
 
@@ -58,43 +81,19 @@ export default class Widget extends Component {
 
 
     render(){
-        const {position} = this.state
-        const {howleft, input} = this.props
+        const {position, when, clicked} = this.state
+        const {howleft, input, clickeddata} = this.props
 
         return(
-            <div class = {widget_style.box} style={{position: "absolute", top: position, left:howleft }}>
-                <p>
+
+            <div class = {widget_style.box} 
+            style={{position: "absolute", top: position, left:howleft }}
+            onClick={this.startclick}
+            >
                     {input}
-                </p>
             </div>
 
         )
 
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
