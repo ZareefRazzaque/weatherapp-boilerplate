cb105907d436afd042f9417bac0a4fdbb8af1544
shenikareddy <shen.reddy14@gmail.com>
2023-03-23 13:18:22 +0000

    started on alerts

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -313,6 +313,8 @@ export default class Iphone extends Component {
 
         let alerts = <div> 
                 <div> Weather Alerts</div> 
+                <div><img src='.../.../assets/icons/warning-sign.png' style='width:50px;height:50px;'></img></div>
+                <div>EARTHQUAKE WARNING (under construction)</div>
             </div>
 
 
